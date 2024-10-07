a97f8a653c4a057a4b3244383e4a69909c35ae02
Daniel Moon <daniel_moon16@yahoo.com>
2024-10-07 17:25:46 -0400

    adding disclaimer and use guide to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FileSelectorComponent } from './file-selector/file-selector.component';
 import { MetadataDisplayComponent } from './metadata-display/metadata-display.component';
 import { DownloaderComponent } from './downloader/downloader.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FileSelectorComponent, MetadataDisplayComponent, DownloaderComponent, MatSidenavModule],
+  imports: [RouterOutlet, FileSelectorComponent, MetadataDisplayComponent, DownloaderComponent, MatSidenavModule, DisclaimerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
